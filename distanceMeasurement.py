ab2b292b1afcff958ee8acabda2fe6a5f5c45447
William <gilliamwlover@gmail.com>
2021-09-10 14:22:55 -0700

    mapping output from drone

--- distanceMeasurement.py
+++ distanceMeasurement.py
@@ -31,27 +31,27 @@ def getTelemetry():
         
         response is: 
         {
-            'mid': -1, 
-            'x': -100, 
-            'y': -100, 
-            'z': -100, 
-            'mpry': '0,0,0', 
-            'pitch': 0, 
-            'roll': -1, 
-            'yaw': 5, 
-            'vgx': 0, 
-            'vgy': 0, 
-            'vgz': 0, 
-            'templ': 56, 
-            'temph': 59, 
-            'tof': 10, 
-            'h': 0, 
-            'bat': 6, 
-            'baro': 412.3, 
-            'time': 0, 
-            'agx': 0.0, 
-            'agy': 20.0, 
-            'agz': -998.0
+            'mid': -1,          this is mission pad related
+            'x': -100,          this is mission pad related
+            'y': -100,          this is mission pad related
+            'z': -100,          this is mission pad related
+            'mpry': '0,0,0',    this is x, y and z all in one
+            'pitch': 0,         pitch in degrees
+            'roll': -1,         roll in degrees
+            'yaw': 5,           yaw in degrees
+            'vgx': 0,           speed_x
+            'vgy': 0,           speed_y
+            'vgz': 0,           speed_z
+            'templ': 56,        temp_low °C 
+            'temph': 59,        temp_high °C
+            'tof': 10,          tof distance
+            'h': 0,             height in cm
+            'bat': 6,           battery percent
+            'baro': 412.3,      barometer value in cm -abs height
+            'time': 0,          flight time in s
+            'agx': 0.0,         acceleration_x
+            'agy': 20.0,        acceleration_y
+            'agz': -998.0       acceleration_z
         }
         
         '''
