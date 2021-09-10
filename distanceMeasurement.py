ad26dae717b1b0e2ab9e1f28317d2de82a656973
William <gilliamwlover@gmail.com>
2021-09-09 19:41:10 -0700

    added get_current_state() output

--- distanceMeasurement.py
+++ distanceMeasurement.py
@@ -22,11 +22,38 @@ now = []
 
 drone = Tello()
 drone.connect()
+print(drone.get_current_state())
 
 def getTelemetry():
     while True:
         '''
         This is going to run in a loop for the entire duration of the flight. Need to research into "get_current_state()" to see if it will just return all these values instead of doing all these calls a bunch of times, just do the one call, update the information, and repeat 
+        
+        response is: 
+        {
+            'mid': -1, 
+            'x': -100, 
+            'y': -100, 
+            'z': -100, 
+            'mpry': '0,0,0', 
+            'pitch': 0, 
+            'roll': -1, 
+            'yaw': 5, 
+            'vgx': 0, 
+            'vgy': 0, 
+            'vgz': 0, 
+            'templ': 56, 
+            'temph': 59, 
+            'tof': 10, 
+            'h': 0, 
+            'bat': 6, 
+            'baro': 412.3, 
+            'time': 0, 
+            'agx': 0.0, 
+            'agy': 20.0, 
+            'agz': -998.0
+        }
+        
         '''
         height = drone.get_height()
         pitch = drone.get_pitch()
@@ -41,9 +68,11 @@ def getTelemetry():
         now = time.time()
 
 
+
 def moveForward():
-    drone.takeoff()
-    drone.get_current_state()
-    time.sleep(2) #Should be enough time to get off the ground
-    drone.move(10)
-    drone.land()
+    #drone.takeoff()
+    drone = Tello()
+    drone.connect()
+    print(drone.get_current_state())
+       
+    
